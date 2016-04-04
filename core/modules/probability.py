b7d01fb1629dd40d6d4072c25571c7f5de8d99be
Luis Pablo <luismpablo@gmail.com>
2016-04-03 23:58:11 -0400

    Fix float bug on probability calc

--- core/modules/probability.py
+++ core/modules/probability.py
@@ -1,5 +1,7 @@
 def pairwise_probability_calculation(p_a, p_b):
-    pr_a = 1 / float((10 ** -(p_a.rating - p_b.rating) / float(400)) + 1)
+    elo_diff = p_a.rating - p_b.rating
+    elo_diff_div_400 = -elo_diff / float(400)
+    pr_a = 1 / (float(10 ** elo_diff_div_400) + 1)
 
     win_p_a = pr_a
     lose_p_b = pr_a
@@ -8,7 +10,7 @@ def pairwise_probability_calculation(p_a, p_b):
 
     return {
         p_a.player_id: {
-            'win': win_p_a,
+            'win': float(win_p_a),
             'lose': lose_p_a
         },
         p_b.player_id: {
