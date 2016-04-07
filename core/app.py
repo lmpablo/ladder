a02040ba7ec3077958b550675c278acd8c0926f8
Jaime Barrita <jab.colo33@gmail.com>
2016-04-07 00:03:47 -0400

    Add match endpoint & index and matches view

--- core/app.py
+++ core/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask.json import jsonify
 from mongokit import Connection
 from mongokit.schema_document import SchemaDocumentError
@@ -6,8 +6,12 @@ from config import DB_HOST, DB_NAME, DB_PORT
 from models.match import Match
 from models.player import Player
 from modules.probability import pairwise_probability_calculation as win_probability
+import json
 
-app = Flask(__name__)
+import datetime
+from dateutil import parser
+
+app = Flask(__name__, template_folder='../frontend/')
 app.config.from_pyfile('config.py')
 
 
@@ -29,15 +33,32 @@ def missing(field_list):
 
 
 @app.route("/")
-def hello():
-    return json_response()
+def index():
+    return render_template('index.html')
 
+
+@app.route("/matches")
+def matches_view():
+    # TODO: Access the API instead of the function
+    matches = json.loads(get_matches()[0].get_data())
+    return render_template('matches/list.html', matches=matches)
+
+# Players
+
+@app.route("/api/v1/players/<player_id>")
+def get_player(player_id):
+    user = db.Player.find_one({'player_id': player_id})
+
+    if user is None:
+        return json_response(data={}, code=400, status="error", reason="User Not Found")
+    else:
+        res = user
+        return json_response(res.to_json_type())
 
 @app.route("/api/v1/players")
 def get_players():
     users = list(db.Player.find())
     return json_response(data={'users': [u.to_json_type() for u in users]})
-
 
 @app.route("/api/v1/players", methods=['POST'])
 def add_player():
@@ -57,25 +78,47 @@ def add_player():
     except SchemaDocumentError, e:
         return json_response(reason="Validation Error", data=str(e), status="error", code=400)
 
+# Matches
+
+@app.route("/api/v1/matches/<match_id>")
+def get_match(match_id):
+    match = db.Match.find_one({'match_id': match_id})
+    print type(match)
+
+    if match is None:
+        return json_response(data={}, code=400, status="error", reason="Match Not Found")
+    else:
+        res = match
+        return json_response(res.to_json_type())
 
 @app.route("/api/v1/matches")
 def get_matches():
     matches = list(db.Match.find({}, {'_id': 0}))
     return json_response(data={'matches': matches})
 
+@app.route("/api/v1/matches", methods=['POST'])
+def add_match():
+    json_request = request.get_json()
+    match = db.Match()
 
-@app.route("/api/v1/players/<player_id>")
-def get_player(player_id):
-    user = db.Player.find_one({'player_id': player_id})
-    print type(user)
+    for key, val in json_request.iteritems():
+        if key not in match.viewkeys():
+            continue
+        if key == "timestamp":
+            match[key] = parser.parse(val)
+        else:
+            match[key] = val
+    try:
+        match.validate()
+        if db.Match.find_one({'match_id': json_request['match_id']}) is not None:
+            return json_response(status="error", reason="Match already exists", code=400)
+        match.save()
+        return json_response()
+    except SchemaDocumentError, e:
+        return json_response(reason="Validation Error", data=str(e), status="error", code=400)
 
-    if user is None:
-        return json_response(data={}, code=400, status="error", reason="User Not Found")
-    else:
-        res = user
-        return json_response(res.to_json_type())
 
-
+# Player <-> Match
 @app.route("/api/v1/players/<player_id>/matches", methods=['GET'])
 def get_player_matches(player_id):
     matches = list(db.Match.find({'participants.player_id': {'$in': [player_id]}}))
